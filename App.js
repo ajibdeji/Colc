f130b1de479e06a1dad7d7330646ab0db6bacec4
ajibdeji <ajibdeji@gmail.com>
2018-05-10 00:07:53 -0400

    redux set up

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import { StyleSheet, Text, View } from 'react-native';
+import reducers from './src/reducers'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>Hello</Text>
+        </View>  
+      </Provider>
     );
   }
 }
